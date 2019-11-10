6a99d703709838e865951e3a813f17efa6d5149d
Prem Kumar <premku@vmware.com>
2019-11-10 15:02:15 +0530

    pushed changes for classreader

---
+++ ClassPropertyReader/ClassPropertyReader/ClassReader.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassPropertyReader
+{
+    public class ClassReader
+    {
+        public static StringBuilder Read(Object obj)
+        {
+            // Reading properties of object through reflection
+            var props = obj.GetType().GetProperties();
+            var sb = new StringBuilder();
+            foreach (var p in props)
+            {
+                sb.Append(p.Name + p.GetValue(obj, null));
+            }
+            return sb;
+            
+        }
+    }
+}
---
+++ ClassPropertyReader/ClassPropertyReader/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+namespace ClassPropertyReader
+{
+   
+    class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Salary { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sb = ClassReader.Read(new Employee());
+            Console.WriteLine(sb);
+        
+    }
+    }
+}
+
+
